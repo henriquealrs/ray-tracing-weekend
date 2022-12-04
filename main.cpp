a349a954ba0dbecf4e275b4f5de0b407a7c6a690
henriquealrs <henriquealrs@gmail.com>
2022-12-04 12:13:29 -0300

    Unity_Vector() test

--- main.cpp
+++ main.cpp
@@ -61,7 +61,7 @@ int main() {
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            Color pixel_color;
+            Color pixel_color(0, 0, 0);
             for(int i = 0; i < samples_per_pixel; ++i) {
                 auto u = (i + random_double<double>()) / (image_width - 1);
                 auto v = (j + random_double<double>()) / (image_height - 1);
--- tests/vec3_tests.cpp
+++ tests/vec3_tests.cpp
@@ -16,3 +16,12 @@ TEST(Vec3, Sum) {
 TEST(Vec3, division) {
     ASSERT_TRUE((vec3(3, 4, 5) / 10) == vec3(.3, .4, .5));
 }
+
+
+TEST(Vec3, unity_vector) {
+    vec3 orig(1, 2, 3);
+    auto u = unit_vector(orig);
+
+    ASSERT_TRUE(u.length_squared() == 1);
+    ASSERT_TRUE( cross(u, orig).length_squared() < 0.001 );
+}
